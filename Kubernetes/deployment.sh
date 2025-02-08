38db6f75a5687b22139d914792567a356bf6b4d8
Icaro Sousa <icaro.py@gmail.com>
2025-02-08 14:17:47 +0000

    renameconfig map and removing  dry-run

---
+++ Kubernetes/deployment.sh
@@ -0,0 +1,11 @@
+kubectl apply -f configmap-rabbitmq.yaml 
+kubectl apply -f configmap-postgresql.yaml 
+kubectl apply -f configmap-order-service.yaml 
+kubectl apply -f configmap-process-service.yaml 
+kubectl apply -f configmap-indetity-service.yaml
+
+kubectl apply -f deployment-rabbitmq.yaml 
+kubectl apply -f deployment-postgresql.yaml 
+kubectl apply -f deployment-order-service.yaml 
+kubectl apply -f deployment-process-service.yaml 
+kubectl apply -f deployment-identity-service.yaml 
---
+++ Kubernetes/rollback.sh
@@ -0,0 +1,11 @@
+kubectl delete -f configmap-rabbitmq.yaml 
+kubectl delete -f configmap-postgresql.yaml 
+kubectl delete -f configmap-order-service.yaml 
+kubectl delete -f configmap-process-service.yaml 
+kubectl delete -f configmap-indetity-service.yaml
+
+kubectl delete -f deployment-rabbitmq.yaml 
+kubectl delete -f deployment-postgresql.yaml 
+kubectl delete -f deployment-order-service.yaml 
+kubectl delete -f deployment-process-service.yaml 
+kubectl delete -f deployment-identity-service.yaml 
