97710d37e7459186e5fb418dac389f6a463cbe34
Icaro Sousa <icaro.py@gmail.com>
2025-02-08 17:49:48 +0000

    setup basic k8s

--- Kubernetes/deployment.sh
+++ Kubernetes/deployment.sh
@@ -1,11 +1,13 @@
-kubectl apply -f configmap-rabbitmq.yaml --validate=false
-kubectl apply -f configmap-postgresql.yaml --validate=false
-kubectl apply -f configmap-order-service.yaml --validate=false
-kubectl apply -f configmap-process-service.yaml --validate=false
+kubectl apply -f configmap-rabbitmq.yaml
+kubectl apply -f configmap-postgresql.yaml
+kubectl apply -f configmap-order-service.yaml
+kubectl apply -f configmap-process-service.yaml
 kubectl apply -f configmap-indetity-service.yaml
 
-kubectl apply -f deployment-rabbitmq.yaml --validate=false
-kubectl apply -f deployment-postgresql.yaml --validate=false
-kubectl apply -f deployment-order-service.yaml --validate=false
-kubectl apply -f deployment-process-service.yaml --validate=false
-kubectl apply -f deployment-identity-service.yaml --validate=false
+kubectl apply -f vpc-rabbitmq.yaml
+kubectl apply -f deployment-rabbitmq.yaml
+kubectl apply -f vpc-postgresql.yaml
+kubectl apply -f deployment-postgresql.yaml
+kubectl apply -f deployment-order-service.yaml
+kubectl apply -f deployment-process-service.yaml
+kubectl apply -f deployment-identity-service.yaml
--- Kubernetes/rollback.sh
+++ Kubernetes/rollback.sh
@@ -5,7 +5,9 @@ kubectl delete -f configmap-process-service.yaml
 kubectl delete -f configmap-indetity-service.yaml
 
 kubectl delete -f deployment-rabbitmq.yaml 
-kubectl delete -f deployment-postgresql.yaml 
+kubectl delete -f vpc-rabbitmq.yaml
+kubectl delete -f deployment-postgresql.yaml
+kubectl delete -f vpc-postgresql.yaml
 kubectl delete -f deployment-order-service.yaml 
 kubectl delete -f deployment-process-service.yaml 
 kubectl delete -f deployment-identity-service.yaml 
